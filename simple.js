f726aa7a8692b9055dccff9ac91d015c646c952a
0jbaumstark <42749684+0jbaumstark@users.noreply.github.com>
2018-08-31 13:53:59 -0500

    Bang, Update 1

---
+++ simple.js
@@ -0,0 +1,17 @@
+//Your savings decreases as you spend money...
+let bankAccount = 1000000000
+const rent = 600000;
+const carPayment = 25000;
+
+//Guess what this will do!
+bankAccount = bankAccount - 12*rent;
+console.log(bankAccount);
+//Did you get what you expect?
+//Write a new statement below which subtracts 5 years of a car payment on line 12.
+
+
+//Declare a variable named income on line 15, assign yourself 2500 in income.
+
+//On the next line
+
+
---
+++ vars.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = -25;
+//create two more variables c and d and give them whatever number you want.
+
+
+const total = a+b+c+d;
+console.log(total);
+
+
+
+
+//new section on string variables
+let convo = "hello ";
+//set name to your name, first and last.
+let name = Joshua;
+//add string convo and name together.
+const greeting = Hello
+console.log(greeting);
+
+//make your own message similar to what is above.
+let convo2;
+let name2 = Mr. Fleming
+const greeting = Good how are you
+console.log(greeting);
